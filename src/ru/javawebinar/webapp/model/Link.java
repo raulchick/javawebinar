be5bc11350a30728283f38d7d3fd2721144ec184
raulchick <szadiraka@mail.ru>
2017-02-10 19:30:28 +0200

    Link class: add methods equals() and heshcode()

--- src/ru/javawebinar/webapp/model/Link.java
+++ src/ru/javawebinar/webapp/model/Link.java
@@ -13,4 +13,23 @@ public class Link {
         this.name = link.name;
         this.url = link.url;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        if (!name.equals(link.name)) return false;
+        return url != null ? url.equals(link.url) : link.url == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
+    }
 }
